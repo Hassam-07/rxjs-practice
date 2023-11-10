97384d693d6ed18d607114f607ae1b39a0003890
Hassam <101381873+Hassam-07@users.noreply.github.com>
2023-11-10 18:18:26 +0500

    rxjs exercises code updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,29 @@
-import { Component } from '@angular/core';
-import { flatMap, map, mergeMap, of, from, reduce } from 'rxjs';
-import { concatMap, zip } from 'rxjs/operators';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  flatMap,
+  map,
+  mergeMap,
+  of,
+  from,
+  reduce,
+  fromEvent,
+  Subject,
+} from 'rxjs';
+import {
+  concatMap,
+  zip,
+  filter,
+  toArray,
+  tap,
+  takeUntil,
+  take,
+} from 'rxjs/operators';
 
+interface PriceRecord {
+  name: string;
+  timeStamp: Date;
+  // Add other properties as per your actual data structure
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,7 +31,9 @@ import { concatMap, zip } from 'rxjs/operators';
 })
 export class AppComponent {
   title = 'os-rxjs-practice';
-
+  pricesNASDAQ: PriceRecord[] = [];
+  private unsubscribe$ = new Subject<void>();
+  constructor(private buttonRef: ElementRef) {}
   // Exercise 1
 
   function1() {
@@ -938,5 +962,365 @@ export class AppComponent {
       });
     console.log(videoIdAndBookmarkIdPairs);
     return videoIdAndBookmarkIdPairs;
+  }
+
+  function24() {
+    const movieLists = [
+      {
+        name: 'New Releases',
+        videos: [
+          {
+            id: 70111470,
+            title: 'Die Hard',
+            boxarts: [
+              {
+                width: 150,
+                height: 200,
+                url: 'http://cdn-0.nflximg.com/images/2891/DieHard150.jpg',
+              },
+              {
+                width: 200,
+                height: 200,
+                url: 'http://cdn-0.nflximg.com/images/2891/DieHard200.jpg',
+              },
+            ],
+            url: 'http://api.netflix.com/catalog/titles/movies/70111470',
+            rating: 4.0,
+            interestingMoments: [
+              { type: 'End', time: 213432 },
+              { type: 'Start', time: 64534 },
+              { type: 'Middle', time: 323133 },
+            ],
+          },
+          {
+            id: 654356453,
+            title: 'Bad Boys',
+            boxarts: [
+              {
+                width: 200,
+                height: 200,
+                url: 'http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg',
+              },
+              {
+                width: 140,
+                height: 200,
+                url: 'http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg',
+              },
+            ],
+            url: 'http://api.netflix.com/catalog/titles/movies/70111470',
+            rating: 5.0,
+            interestingMoments: [
+              { type: 'End', time: 54654754 },
+              { type: 'Start', time: 43524243 },
+              { type: 'Middle', time: 6575665 },
+            ],
+          },
+        ],
+      },
+      {
+        name: 'Instant Queue',
+        videos: [
+          {
+            id: 65432445,
+            title: 'The Chamber',
+            boxarts: [
+              {
+                width: 130,
+                height: 200,
+                url: 'http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg',
+              },
+              {
+                width: 200,
+                height: 200,
+                url: 'http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg',
+              },
+            ],
+            url: 'http://api.netflix.com/catalog/titles/movies/70111470',
+            rating: 4.0,
+            interestingMoments: [
+              { type: 'End', time: 132423 },
+              { type: 'Start', time: 54637425 },
+              { type: 'Middle', time: 3452343 },
+            ],
+          },
+          {
+            id: 675465,
+            title: 'Fracture',
+            boxarts: [
+              {
+                width: 200,
+                height: 200,
+                url: 'http://cdn-0.nflximg.com/images/2891/Fracture200.jpg',
+              },
+              {
+                width: 120,
+                height: 200,
+                url: 'http://cdn-0.nflximg.com/images/2891/Fracture120.jpg',
+              },
+              {
+                width: 300,
+                height: 200,
+                url: 'http://cdn-0.nflximg.com/images/2891/Fracture300.jpg',
+              },
+            ],
+            url: 'http://api.netflix.com/catalog/titles/movies/70111470',
+            rating: 5.0,
+            interestingMoments: [
+              { type: 'End', time: 45632456 },
+              { type: 'Start', time: 234534 },
+              { type: 'Middle', time: 3453434 },
+            ],
+          },
+        ],
+      },
+    ];
+
+    return from(movieLists)
+      .pipe(
+        concatMap((movieList) =>
+          from(movieList.videos).pipe(
+            concatMap((video) =>
+              from(video.boxarts).pipe(
+                reduce((acc, curr) =>
+                  acc.width * acc.height < curr.width * curr.height ? acc : curr
+                ),
+                concatMap((boxart) =>
+                  from(video.interestingMoments).pipe(
+                    filter(
+                      (interestingMoment: any) =>
+                        interestingMoment.type === 'Middle'
+                    ),
+                    zip(from([boxart]), (moment: any, boxart) => ({
+                      id: video.id,
+                      title: video.title,
+                      time: moment.time,
+                      url: boxart.url,
+                    }))
+                  )
+                )
+              )
+            )
+          )
+        )
+      )
+      .subscribe((result) => console.log(result));
+  }
+
+  // Exercise 25
+
+  function25() {
+    const lists = [
+      {
+        id: 5434364,
+        name: 'New Releases',
+      },
+      {
+        id: 65456475,
+        name: 'Thrillers',
+      },
+    ];
+
+    const videos = [
+      {
+        listId: 5434364,
+        id: 65432445,
+        title: 'The Chamber',
+      },
+      {
+        listId: 5434364,
+        id: 675465,
+        title: 'Fracture',
+      },
+      {
+        listId: 65456475,
+        id: 70111470,
+        title: 'Die Hard',
+      },
+      {
+        listId: 65456475,
+        id: 654356453,
+        title: 'Bad Boys',
+      },
+    ];
+
+    return from(lists)
+      .pipe(
+        map((list) => {
+          const filteredVideos = videos.filter(
+            (video) => video.listId === list.id
+          );
+          const videosArr = filteredVideos.map((video) => ({
+            id: video.id,
+            title: video.title,
+          }));
+          return { name: list.name, videos: videosArr };
+        }),
+        toArray()
+      )
+      .subscribe((result) => console.log(result));
+  }
+
+  // Exercise 26
+
+  function26() {
+    const lists = [
+      {
+        id: 5434364,
+        name: 'New Releases',
+      },
+      {
+        id: 65456475,
+        name: 'Thrillers',
+      },
+    ];
+
+    const videos = [
+      {
+        listId: 5434364,
+        id: 65432445,
+        title: 'The Chamber',
+      },
+      {
+        listId: 5434364,
+        id: 675465,
+        title: 'Fracture',
+      },
+      {
+        listId: 65456475,
+        id: 70111470,
+        title: 'Die Hard',
+      },
+      {
+        listId: 65456475,
+        id: 654356453,
+        title: 'Bad Boys',
+      },
+    ];
+
+    const boxarts = [
+      {
+        videoId: 65432445,
+        width: 130,
+        height: 200,
+        url: 'http://cdn-0.nflximg.com/images/2891/TheChamber130.jpg',
+      },
+      {
+        videoId: 65432445,
+        width: 200,
+        height: 200,
+        url: 'http://cdn-0.nflximg.com/images/2891/TheChamber200.jpg',
+      },
+      {
+        videoId: 675465,
+        width: 200,
+        height: 200,
+        url: 'http://cdn-0.nflximg.com/images/2891/Fracture200.jpg',
+      },
+      {
+        videoId: 675465,
+        width: 120,
+        height: 200,
+        url: 'http://cdn-0.nflximg.com/images/2891/Fracture120.jpg',
+      },
+      {
+        videoId: 675465,
+        width: 300,
+        height: 200,
+        url: 'http://cdn-0.nflximg.com/images/2891/Fracture300.jpg',
+      },
+      {
+        videoId: 70111470,
+        width: 150,
+        height: 200,
+        url: 'http://cdn-0.nflximg.com/images/2891/DieHard150.jpg',
+      },
+      {
+        videoId: 70111470,
+        width: 200,
+        height: 200,
+        url: 'http://cdn-0.nflximg.com/images/2891/DieHard200.jpg',
+      },
+      {
+        videoId: 654356453,
+        width: 200,
+        height: 200,
+        url: 'http://cdn-0.nflximg.com/images/2891/BadBoys200.jpg',
+      },
+      {
+        videoId: 654356453,
+        width: 140,
+        height: 200,
+        url: 'http://cdn-0.nflximg.com/images/2891/BadBoys140.jpg',
+      },
+    ];
+
+    const bookmarks = [
+      { videoId: 65432445, time: 32432 },
+      { videoId: 675465, time: 3534543 },
+      { videoId: 70111470, time: 645243 },
+      { videoId: 654356453, time: 984934 },
+    ];
+
+    return from(lists)
+      .pipe(
+        mergeMap((list) => {
+          const filteredVideos = videos.filter(
+            (video) => video.listId === list.id
+          );
+          const videosArr = filteredVideos.map((video) => {
+            const bookmark = bookmarks.find((b) => b.videoId === video.id);
+            const smallestBoxart = boxarts
+              .filter((b) => b.videoId === video.id)
+              .sort((a, b) => a.width * a.height - b.width * b.height)[0];
+            return {
+              id: video.id,
+              title: video.title,
+              time: bookmark ? bookmark.time : null,
+              boxart: smallestBoxart ? smallestBoxart.url : null,
+            };
+          });
+          return of({
+            name: list.name,
+            videos: videosArr,
+          });
+        }),
+        toArray()
+      )
+      .subscribe((result) => console.log(result));
+  }
+
+  // Exercise 27
+
+  // Exercise 28
+
+  function28() {
+    const button = this.buttonRef.nativeElement;
+
+    const click$ = fromEvent(button, 'click').pipe(
+      takeUntil(this.unsubscribe$)
+    );
+
+    click$.subscribe(() => {
+      alert('Button was clicked. Unsubscribing from event.');
+      this.unsubscribe$.next();
+      this.unsubscribe$.complete();
+    });
+  }
+
+  // Exercise 29
+  function29() {
+    const buttonClicks$ = fromEvent(this.buttonRef.nativeElement, 'click');
+
+    buttonClicks$.pipe(take(1)).subscribe(() => {
+      alert('Button was clicked. Stopping Traversal.');
+    });
+  }
+  // Exercise 20
+  function30() {
+    const buttonClicks$ = fromEvent(this.buttonRef.nativeElement, 'click');
+
+    buttonClicks$.pipe(take(1)).subscribe(() => {
+      alert('Button was clicked once. Stopping Traversal.');
+    });
   }
 }
